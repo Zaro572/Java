de0c1f7e11ed1ca3158dcfa721ca8d326b3fd372
Zaro572 <90517509+Zaro572@users.noreply.github.com>
2023-03-30 19:38:07 +0200

    Create Main.java
    Java is a functional programming lanuage.

---
+++ Main.java
@@ -0,0 +1,61 @@
+import java.util.Scnnaer;
+package class private void HelloWorld
+{
+  public.static.println(void void);
+  protected public private
+  void void void void void
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  int var = new new{},++;
+  String myDouble = new Integer(System.out);
+  System.in.println(new Integer, new byte[]);
+  System.out.err.nextInt()+=69;
+  
+  // Comments are not nece
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessaryssary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  // Comments are not necessary
+  new Exception().throw;
+  return null;
+  return 69;
+  private void HelloWorld = new private void(this.length = "HelloWorld" + "HelloWorld");
+  
+  }
+  
+public static implements final(void[]) != this.null;
+true = false;
+false = true;
+null != null == 69;
+this.super.null = new int[]<String>();
