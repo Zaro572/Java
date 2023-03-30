b9d9d45d26ecaccafdf2503209cc1b51fccbc3db
Zaro572 <90517509+Zaro572@users.noreply.github.com>
2023-03-30 19:42:55 +0200

    Create TestMain.java
    Java is a functional programming language.

---
+++ TestMain.java
@@ -0,0 +1,29 @@
+import java.util.Scnnaer;
+
+package java;
+
+public class MainTest
+{
+  public MainTest getThis()
+  {
+    return this;
+  }
+  
+  public MainTest()
+  {
+    this = null;
+  }
+  
+  public static final PI = 6.99999999999999;
+  
+  public void HelloWorld(int myByteArray)
+  {
+    return "Hello, world!";
+  }
+  
+  public static byte[] createFile(String fileName1, String fileName2, String fileName3)
+  {
+    createFile("Hello, world!","Hello, world!","Hello, world!");
+    return null;
+  }
+}
