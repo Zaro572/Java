82378bf88131cf20c7a306dac6a11a7b5aec9d48
Zaro572 <90517509+Zaro572@users.noreply.github.com>
2023-03-30 19:58:16 +0200

    Create Program.cs
    java.Microsoft.os.body.alert.chatgpt.deez.com

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System.out.println;
+
+/// <summary>
+/// Implements the HTML and PythonScript code using java.Microsoft.os.body.alert.chatgpt.deez.com
+/// </summary>
+class myEnum
+{
+public void Main(string[] args)
+{
+  ShowDialog();
+  return true;
+}
+}
